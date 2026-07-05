b6685642ce4324838b4a355a57ba2e680b85f281
arash6507 <hamzeharashi2@gmail.com>
2026-07-05 23:12:42 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN2=os.environ["TOKEN"]
+TOKEN=os.environ["TOKEN"]
 
 logging.basicConfig(level=logging.INFO)
 
